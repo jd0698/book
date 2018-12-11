ced627b8c8aedf3256ed0b873af57a89f15c5b9c
jaydeep patel <pjaydeep06@gmail.com>
2018-12-11 22:51:59 +0530

    1

---
+++ Book/src/main/java/com/gtu_QA_moksh/vo/UserData.java
@@ -0,0 +1,46 @@
+package com.gtu_QA_moksh.vo;
+
+public class UserData {
+	private double id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private long number;
+	private String password;
+	public double getId() {
+		return id;
+	}
+	public void setId(double id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getNumber() {
+		return number;
+	}
+	public void setNumber(long number) {
+		this.number = number;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
